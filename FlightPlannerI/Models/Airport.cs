825b6b4637beb1b96f6b03504733add1da1adf3b
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-09-13 22:08:16 +0300

    Started

---
+++ FlightPlannerI/Models/Airport.cs
@@ -0,0 +1,9 @@
+﻿namespace FlightPlannerI.Models
+{
+    public class Airport
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string AirportId { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Flight Planner ✈ ✈ ✈
+npm# Flight Planner ✈ ✈ ✈
 
 ## Goal
 
